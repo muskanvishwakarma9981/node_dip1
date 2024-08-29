a698e308775f2ad1d690b712c7c893e0bf8e56f1
muskanvishwakarma9981 <muskanvishwakarma9981@gmail.com>
2024-08-29 16:39:44 +0530

    add new api

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ app.get('/api/getapi', (req, res)=>{
     res.send({message:"node js diployement"})
 })
 
+app.get('/api/get_user', (req, res)=>{
+    res.send({message:"hello aws diployement"})
+})
 
 app.listen(9000, ()=>{
     console.log('serevre is runningn on server')
